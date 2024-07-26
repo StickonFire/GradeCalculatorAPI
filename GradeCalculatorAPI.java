13a45ba0728c8183b8a2c3f1a78b42c98e70792f
StickonFire <70039286+StickonFire@users.noreply.github.com>
2024-07-26 01:21:10 -0700

    Added API versions of the story 1 functions
    Make the API have the story 1's functions to let the user access them.

---
+++ GradeCalculatorAPI.java
@@ -0,0 +1,33 @@
+public class GradeCalculatorAPI {
+    Root assignments;
+    
+    void addAssignment(int points, int totalPoints, String name){
+
+    }
+
+    void addAssignment(int maxPoints, String name){
+
+    }
+
+    void addAssignment(String name){
+
+    }
+
+    boolean checkAssignmentExists(String title){
+        return false;
+    }
+
+    void deleteAssignment(String title){
+        
+    }
+
+    void changePoints(String title, int newPoints){
+
+    }
+
+    void changeTotalPoints(String title, int totalPoints){
+
+    }
+
+
+}
