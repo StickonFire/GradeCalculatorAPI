bd5c1365859466e49a29866abf1a1f9db56ea2bc
StickonFire <70039286+StickonFire@users.noreply.github.com>
2024-08-05 12:07:02 -0700

    Added body to API functions
    Added a body to the API functions given.

--- GradeCalculatorAPI/GradeCalculatorAPI.java
+++ GradeCalculatorAPI/GradeCalculatorAPI.java
@@ -4,31 +4,31 @@ public class GradeCalculatorAPI {
     Root assignments;
 
     void addAssignment(int points, int totalPoints, String name){
-
+        assignments.addAssignment(name, points, totalPoints);
     }
 
     void addAssignment(int maxPoints, String name){
-
+        assignments.addAssignment(name, maxPoints);
     }
 
     void addAssignment(String name){
-
+        assignments.addAssignment(name);
     }
 
     boolean checkAssignmentExists(String title){
-        return false;
+        return assignments.checkAssignmentExists(title);
     }
 
     void deleteAssignment(String title){
-        
+        assignments.removeAssignment(title);
     }
 
     void changePoints(String title, int newPoints){
-
+        assignments.getAssignment(title).changePoints(newPoints);
     }
 
     void changeTotalPoints(String title, int totalPoints){
-
+        assignments.getAssignment(title).changeMaxPoints(totalPoints);
     }
 
 
