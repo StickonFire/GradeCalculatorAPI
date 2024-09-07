d5304dd26de937bf80f1a7de1c921b68ab4d5e35
StickonFire <70039286+StickonFire@users.noreply.github.com>
2024-09-06 19:44:02 -0700

    Added skeletons for the GradeCalculatorAPI tests
    Added skeletons for the following tests:
     - addAssignment(title) where Assignment is in Root
     - addAssignment(points & title) where Assignment is in Root
     - checkAssignmentExists where the assignment is in the root (both true and false)
     - deleteAssignment where it exists and when it doesn't
     - test setPoints where the queried value exists and doesn't
     - test setTotalPoints where the queried Assignment exists or doesn't

--- GradeCalculatorAPI/GradeCalculatorAPITest.java
+++ GradeCalculatorAPI/GradeCalculatorAPITest.java
@@ -4,5 +4,53 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class GradeCalculatorAPITest {
-    
+    @Test
+    public void testAddAssignmentTitleOnlyRoot(){
+        
+    }
+
+    @Test
+    public void testAddAssignmentAllNamedRoot(){
+
+    }
+
+    @Test
+    public void testCheckAssignmentExistsRootTrue(){
+
+    }
+
+    @Test
+    public void testCheckAssignmentExistsRootFalse(){
+
+    }
+
+    @Test
+    public void testRootDeleteAssignmentExists(){
+
+    }
+
+    @Test
+    public void testRootDeleteAssignmentNonexists(){
+
+    }
+
+    @Test
+    public void testRootSetPointsExists(){
+
+    }
+
+    @Test
+    public void testRootSetPointsNonexists(){
+
+    }
+
+    @Test
+    public void testSetTotalPointsRootExists(){
+
+    }
+
+    @Test
+    public void testSetTotalPointsRootNonExists(){
+
+    }
 }
