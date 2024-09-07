a537863b365e460a7c20d55d2802abf2f5da0e5e
StickonFire <70039286+StickonFire@users.noreply.github.com>
2024-09-07 01:07:28 -0700

    implemented tests on addAssignment
    Added tests for both addAssignmentTitleOnlyRoot and testAddAssignmentAllNamedRoot

--- GradeCalculatorAPI/GradeCalculatorAPITest.java
+++ GradeCalculatorAPI/GradeCalculatorAPITest.java
@@ -23,12 +23,23 @@ public class GradeCalculatorAPITest {
 
     @Test
     public void testAddAssignmentTitleOnlyRoot(){
+        Assignment second = new Assignment("second");
+        api.addAssignment("second");
+        Assignment APIsecond = (Assignment)rootContents.get("second");
+        Assignment APIfirst = (Assignment)rootContents.get("first");
+        assertEquals("Checks if addAssignment properly adds an assignment with only a title defined",second,APIsecond);
+        
+        assertEquals("Checks if addAssignment doesn't add too much or subtract from the assignments",rootContents.values().size(),2);
+        assertEquals("Checks if addAssignment doesn't do anything to the already added assignment",first,APIfirst);
         
     }
 
     @Test
     public void testAddAssignmentAllNamedRoot(){
-
+        Assignment second = new Assignment(1,2,"second",false);
+        api.addAssignment("second",1,2,false);
+        Assignment APIsecond = (Assignment)rootContents.get("second");
+        assertEquals("Checks if addAssignment doesn't add too much or subtract from the assignment",rootContents.values().size(),3);
     }
 
     @Test
