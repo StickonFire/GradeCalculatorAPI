b5082ba0dc7664b8ccf66074bdadc04c8d74e3d6
StickonFire <70039286+StickonFire@users.noreply.github.com>
2024-09-20 01:31:55 -0700

    Added Root tests to GetGradeTest.java
    Added tests for the Root in the GetGradeTest.java file.
    
    This includes a full Root with both current grading styles, and an empty Root with both current grading styles.

--- GradeCalculatorAPI/GetGradeTest.java
+++ GradeCalculatorAPI/GetGradeTest.java
@@ -9,6 +9,7 @@ public class GetGradeTest {
 
     private Assignment first;
     private Assignment second;
+    private Assignment third;
     private HashMap<String,Node> rootContents;
     private Root root;
 
@@ -16,10 +17,13 @@ public class GetGradeTest {
     public void setUp(){
         first = new Assignment(1,2,"first",true);
         second = new Assignment(3,5,"second",false);
+        third = new Assignment(2,2,"third",true);
         rootContents = new HashMap<String,Node>();
         root = new Root (rootContents);
         root.addAssignment(first);
         root.addAssignment(second);
+        root.addAssignment(third);
+        root.setGradingStyle(GradingStyle.PointSystemAddUp);
     }
     //Test Assignment get grade
     @Test
@@ -34,7 +38,52 @@ public class GetGradeTest {
     }
     
     //Test Root get grade
+    @Test
+    public void testRootGetGrade1Ignored3TotalPointSystemAddUp(){
+        assertEquals("Make sure that the root correctly ignores 1, and successfully adds the two others",new Fraction(3,4),root.getGrade());
+    }
 
+    @Test
+    public void testRootGetGrade1Ignored3TotalPointSystemGiven(){
+        root.setGradingStyle(GradingStyle.PointSystemTotalDefined);
+        root.totalPoints = 15;
+        assertEquals("Make sure that the root correctly ignores 1, and successfully adds the two others",new Fraction(3,15),root.getGrade());
+    }
+
+    @Test
+    public void testRootGetGradeEmptyPointSytemAddUp(){
+        Root emptyRoot = new Root(new HashMap<String,Node>());
+
+        boolean noError = true;
+        Fraction result = null;
+
+        try{
+            result = emptyRoot.getGrade();
+        } catch (Exception e){
+            noError = false;
+        }
+
+        assertTrue("Make sure that no error is triggered",noError);
+        assertEquals("Make sure that an emptyRoot(with added up totalPoints) provides the correct answer.",new Fraction(0,0),result);
+    }
+    
+    @Test
+    public void testRootGetEmptyPointSystemGiven(){
+        Root emptyRoot = new Root(new HashMap<String,Node>(),GradingStyle.PointSystemTotalDefined);
+        emptyRoot.setTotalPoints(15);
+
+        boolean noError = true;
+        Fraction result = null;
+
+        try{
+            result = emptyRoot.getGrade();
+        } catch(Exception e){
+            noError = false;
+        }
+
+        assertTrue("Make sure that no error is triggered",noError);
+        assertEquals("Make sure that an emptyRoot(with defined totalPoints) provides the correct answer.",new Fraction(0,15),result);
+    }
 
     //GradeCalculatorAPI test getGrade
     @Test 
